c61b34c027f286201db881f31bffb904eacbb3f7
Varun Sahni <varunsahni@tamu.edu>
2024-04-21 18:26:03 -0500

    Adding Users to DB without sex, orientation, and last active on.

--- app.py
+++ app.py
@@ -35,13 +35,13 @@ def login():
         return jsonify({'success': False, 'message': 'Invalid credentials'})
 
 # Function to add a new user to the database
-def add_user(username, password, age):
+def add_user(username, password, sex, age, location, status, orientation, body_type, diet, drinks, drugs, education, ethnicity, height, income, job, offspring, pets, religion, sign, smokes, speaks, essay0, essay1, essay2, essay3, essay4, essay5, essay6, essay7, essay8, essay9):
     # Connect to the database
     conn = sqlite3.connect('users.db')
     cursor = conn.cursor()
 
     # Insert the new user into the database
-    cursor.execute("INSERT INTO users (username, password, age) VALUES (?, ?, ?)", (username, password, age))
+    cursor.execute("INSERT INTO users (username, password, sex, age, location, status, orientation, body_type, diet, drinks, drugs, education, ethnicity, height, income, job, offspring, pets, religion, sign, smokes, speaks, essay0, essay1, essay2, essay3, essay4, essay5, essay6, essay7, essay8, essay9) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (username, password, sex, age, location, status, orientation, body_type, diet, drinks, drugs, education, ethnicity, height, income, job, offspring, pets, religion, sign, smokes, speaks, essay0, essay1, essay2, essay3, essay4, essay5, essay6, essay7, essay8, essay9))
 
     # Commit changes and close the database connection
     conn.commit()
@@ -53,12 +53,41 @@ def join():
     data = request.get_json()
     username = data['username']
     password = data['password']
+    sex = data['sex']
     age = data['age']
+    location = data['location']
+    status = data['status']
+    orientation = data['orientation']
+    body_type = data['body_type']
+    diet = data['diet']
+    drinks = data['drinks']
+    drugs = data['drugs']
+    education = data['education']
+    ethnicity = data['ethnicity']
+    height = data['height']
+    income = data['income']
+    job = data['job']
+    offspring = data['offspring']
+    pets = data['pets']
+    religion = data['religion']
+    sign = data['sign']
+    smokes = data['smokes']
+    speaks = data['speaks']
+    essay0 = data['essay0']
+    essay1 = data['essay1']
+    essay2 = data['essay2']
+    essay3 = data['essay3']
+    essay4 = data['essay4']
+    essay5 = data['essay5']
+    essay6 = data['essay6']
+    essay7 = data['essay7']
+    essay8 = data['essay8']
+    essay9 = data['essay9']
 
     if validate_user(username, password):
         return jsonify({'success': False, 'message': 'User already exists'})
     else:
-        add_user(username, password, age)
+        add_user(username, password, sex, age, location, status, orientation, body_type, diet, drinks, drugs, education, ethnicity, height, income, job, offspring, pets, religion, sign, smokes, speaks, essay0, essay1, essay2, essay3, essay4, essay5, essay6, essay7, essay8, essay9)
         return jsonify({'success': True, 'message': 'User added successfully'})
 
 if __name__ == '__main__':
